da839cd6c9e756aeda98a70b48c1401d4919ec87
seyfullah daldal <seyfooo58@gmail.com>
2022-11-24 22:41:17 +0300

    Initial Commit

---
+++ Auto Layout Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Programmatically
+//
+//  Created by Seyfullah Daldal on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
